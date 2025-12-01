1af760633d63b52070618f90dc6676742648cfcf
Melnik <melnikoleg080@gmail.com>
2025-12-01 17:48:35 +0300

    created class card

--- src/main/java/com/example/dungeon/game/Card.java
+++ src/main/java/com/example/dungeon/game/Card.java
@@ -1,4 +1,20 @@
 package com.example.dungeon.game;
 
-public class Card {
+import lombok.Getter;
+import java.io.Serializable;
+
+@Getter
+public class Card implements Serializable {
+    private CardType type;
+    private String name;
+
+    public Card(CardType type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + type + ")";
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.dungeon_mayhem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens com.example.dungeon to javafx.fxml;
